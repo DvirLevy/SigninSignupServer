571a0f18b78f4da9e425c7434a397a65a279d7be
Dvir_Levy <dvir.levy@moovit.com>
2022-07-14 09:54:19 +0300

    add validation to User schema

--- Models/User.js
+++ Models/User.js
@@ -9,27 +9,28 @@ const userSchema = new mongoose.Schema({
     },
     last_name : {
         type : String,
-        required : true,
+        required : [true, "First name is required"],
         min : 2
     },
     email : {
         type : String,
-        require : true,
+        require : [true, "valid email is required"],
         min : 4
     },
     password : {
         type : String,
-        required : true,
+        required : [true, "valid password is required"],
         min : 6
     },
     allow_promotions : {
         type : Boolean,
         required : false
     },
-    date :{
+    date_created :{
         type : Date,
         default : Date.now
     }
+    
 })
 
 module.exports = mongoose.model("User" , userSchema)
